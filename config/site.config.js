5d2769cb962b3fbf54077d6c85c980b8f7c918c0
Tmzncty <72063145+tmzncty@users.noreply.github.com>
2022-04-05 16:09:43 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/128.png',
+  icon: '',
 
   // The name of your website. Present alongside your icon.
   title: "TMZN的搜索站",
